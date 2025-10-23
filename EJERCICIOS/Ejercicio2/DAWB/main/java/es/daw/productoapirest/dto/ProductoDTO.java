c80f0ff251b9d6503ecd86c1fd12cf5ed57bb5c8
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2025-10-23 17:41:13 +0200

    Add files via upload

---
+++ EJERCICIOS/Ejercicio2/DAWB/main/java/es/daw/productoapirest/dto/ProductoDTO.java
@@ -0,0 +1,14 @@
+package es.daw.productoapirest.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductoDTO {
+    private String nombre;
+    private BigDecimal precio;
+    private String codigo;
+    private Integer codigoFabricante;
+
+}
