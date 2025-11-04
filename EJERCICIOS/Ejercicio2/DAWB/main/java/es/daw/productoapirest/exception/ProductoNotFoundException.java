18bee5b8b4e3e93c0a5cf1ce5fca83a7fe8db916
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2025-11-04 17:11:31 +0100

    Delete EJERCICIOS/Ejercicio2/DAWB/main directory

--- EJERCICIOS/Ejercicio2/DAWB/main/java/es/daw/productoapirest/exception/ProductoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package es.daw.productoapirest.exception;
-
-public class ProductoNotFoundException extends RuntimeException{
-    public ProductoNotFoundException(String message){
-        super(message);
-    }
-}
