94f73b93b505d2bb465e7269bebeb2d251bf368f
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2025-11-04 20:25:50 +0100

    Add files via upload

---
+++ EJERCICIOS/Ejercicio2/DAWA/main/java/es/daw/productoapirest/dto/ProductoDTO.java
@@ -0,0 +1,24 @@
+package es.daw.productoapirest.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductoDTO {
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+    @DecimalMin(value = "100.00", message = "El precio debe superior a 99")
+    private BigDecimal precio;
+    @Size(min = 4, max = 4, message = "El codigo debe tener exactamente 4 caracteres")
+    private String codigo;
+
+    // Solo se usa esta propieda al crear un producto, o se devuelve al listar
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Integer codigoFabricante;
+}
---
+++ EJERCICIOS/Ejercicio2/DAWA/main/java/es/daw/productoapirest/exception/ProductoNotFoundException.java
@@ -0,0 +1,8 @@
+package es.daw.productoapirest.exception;
+
+public class ProductoNotFoundException extends RuntimeException{
+    public ProductoNotFoundException(String message) {
+        super(message);
+    }
+
+}
