495f31097ac3222ea033dd31128c721f7071ac99
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2025-11-07 17:10:30 +0100

    Delete EJERCICIOS/Ejercicio2/DAWA/main directory

--- EJERCICIOS/Ejercicio2/DAWA/main/java/es/daw/productoapirest/repository/FabricanteRepository.java
+++
@@ -1,7 +0,0 @@
-package es.daw.productoapirest.repository;
-
-import es.daw.productoapirest.entity.Fabricante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FabricanteRepository extends JpaRepository<Fabricante, Integer> {
-}
